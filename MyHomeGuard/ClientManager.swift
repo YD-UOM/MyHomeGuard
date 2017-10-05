854ee2ab40edb61127beea598d55ccdff7919889
YI DING <yid3@student.unimelb.edu.au>
2017-10-05 14:52:52 +1100

    Initial

---
+++ MyHomeGuard/ClientManager.swift
@@ -0,0 +1,10 @@
+//
+//  ClientManager.swift
+//  MyHomeGuard
+//
+//  Created by YIDING on 29/9/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+class ClientManager {
+    static let sharedClient = MSClient(applicationURLString: "https://myhomeguard.azurewebsites.net")
+}
