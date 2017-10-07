e91e5fdb404faec94ab97d7fe47985d04251311b
YI DING <yid3@student.unimelb.edu.au>
2017-10-07 17:17:36 +1100

    Add Table View Cell

---
+++ MyHomeGuard/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  MyHomeGuard
+//
+//  Created by YIDING on 7/10/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
